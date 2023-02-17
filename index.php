eb45ca81caf6446d0dbf0e8b236a57d5446136d0
DESKTOP-OPF9AJC\Android_PC <mintumia8@gmail.com>
2023-02-17 13:09:45 +0600

    first Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+//echo "Hello World";
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>Home</title>
+</head>
+<body>
+<h1 class="text-success">Hello World!</h1>
+
+
+
+<script src="./javascripts/bootstrap.bundle.min.js"></script>
+</body>
+</html>
